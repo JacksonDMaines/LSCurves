dac614aaa8b389c60f943445ef4512a5942172cd
Jackson D. Maines <95060649+JacksonDMaines@users.noreply.github.com>
2025-03-01 21:18:13 -0600

    added lagtime and differences

--- R/test_script.R
+++ R/test_script.R
@@ -4,7 +4,7 @@ print("test")
 
 x = -10:10
 y = exp(x) / (1 + exp(x))
-z = y + rnorm(length(y), 0,100)
+z = y + rnorm(length(y), 0,1)
 x2 = 0:20
 
 
@@ -15,14 +15,4 @@ plot(x2, y)
 test <- deriv(time = x2, meas = y)
 test
 plot(x2, test)
-
-# first we need to log y
-log_y <- log(y)
-
-derivatives <- c()
-#now you need to start window lm
-for (i in 1:(length(log_y)-1)){
-  slope <- (log_y[i+1] - log_y[i]) / (x[i+1]- y[i])
-  print(slope)
-}
 
--- R/time_func.R
+++ R/time_func.R
@@ -1,27 +1,38 @@
 #git push --mirror https://github.com/JacksonDMaines/LSCurves
 
 # Create function to find derivs? or changes
-# I think ill follow what gcplyr does.
 
-# The idea: fit a lm between a window of points and average?
-#   Will this work well? idk
-
-
-deriv <- function(times, meas, window = 1, blank = 0){
+# First Idea:
+  # Some weird slope function
+deriv <- function(times, meas){
 
   # first we need to log y
   log_y <- log(meas)
 
   ans <- rep(NA, length(meas))
+  ans[1] <- 0
   for (i in 1:(length(log_y)-1)){
-    slope <- ((log_y[i+1] - log_y[i]) / ((times[i+1] - length(times)/2) - meas[i]))
-    ans[i] <- slope
+    slope <- abs(((log_y[i+1] - log_y[i]) / ((times[i+1] - length(times)/2) - meas[i])))
+    ans[i+1] <- slope
   }
   return(ans)
 }
 
+# Second Idea:
+  # Fit lm between points and find rate of change by subtracting last slope
+  # by new slope?
+deriv2 <- function(time, meas, window = 3){
+
+  #First log meas
+
+  #Then window n points
+  #Fit lm model and get slope?
+  #fit next model and differnce slope?
+}
 
 # Create function to find lag time
+  #Fit tangent line to max slope and find intersection to min y value
 
 # Create function to find stationary time
+  #Sliding window with threshold and window parameters
 
