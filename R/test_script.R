7785d27cb3d25abf0715f6819602a0d093796557
Jackson D. Maines <95060649+JacksonDMaines@users.noreply.github.com>
2025-03-02 21:48:32 -0600

    fixed lagtime and started stattime

--- R/test_script.R
+++ R/test_script.R
@@ -4,15 +4,22 @@ print("test")
 
 x = -10:10
 y = exp(x) / (1 + exp(x))
-z = y + rnorm(length(y), 0,1)
+z = y + rnorm(length(y), 0,.02)
 x2 = 0:20
 
 
-plot(x2, y)
+plot(x2, z)
 
 
-
-test <- deriv(time = x2, meas = y)
+test <- deriv(time = x2, meas = z)
 test
 plot(x2, test)
 
+
+test <- differences(time = x2, meas = z, showplot = TRUE)
+test
+
+lag <- lagtime(time = x2, meas = z, differences = test, showplot = TRUE)
+
+plot(x2, z)
+abline(v = 7.966322)
--- R/time_func.R
+++ R/time_func.R
@@ -1,7 +1,6 @@
 #git push --mirror https://github.com/JacksonDMaines/LSCurves
 
 # Create function to find derivs? or changes
-
 # First Idea:
   # Some weird slope function
 deriv <- function(times, meas){
@@ -19,6 +18,23 @@ deriv <- function(times, meas){
 }
 
 # Second Idea:
+# Some differences function
+differences <- function(times, meas, showplot = FALSE){
+
+  ans <- rep(NA, length(meas))
+  ans[1] <- 0
+  for (i in 1:(length(meas)-1)){
+    slope <- meas[i+1] - meas[i]
+    ans[i+1] <- slope
+
+  }
+  if(showplot == TRUE){
+    plot(times, ans, type = "l")
+  }
+  return(ans)
+}
+
+# Third Idea:
   # Fit lm between points and find rate of change by subtracting last slope
   # by new slope?
 deriv2 <- function(time, meas, window = 3){
@@ -30,9 +46,38 @@ deriv2 <- function(time, meas, window = 3){
   #fit next model and differnce slope?
 }
 
+
+
+
+
 # Create function to find lag time
-  #Fit tangent line to max slope and find intersection to min y value
+lagtime <- function(time, meas, differences, showplot = FALSE){
+  # first find max point in differences
+  maxgrowth <- which.max(differences)
+
+  # need slope
+  slope1 <- ((meas[maxgrowth + 1] - meas[maxgrowth]) / (time[maxgrowth + 1] - time[maxgrowth]))
+  slope2 <- ((meas[maxgrowth] - meas[maxgrowth - 1]) / (time[maxgrowth] - time[maxgrowth - 1]))
+  slope <- (slope1 + slope2) / 2
+  #fit tangent line to the point
+  y <- slope * (time - time[maxgrowth]) - meas[maxgrowth]
+    # y−y1=m(x−x1)
+  #find where it intersects with min meas value
+  min_meas <- min(meas)
+  lag <- ((min_meas - meas[maxgrowth]) / slope) + time[maxgrowth]
+
+  if(showplot == TRUE){
+    plot(time, meas)
+    lines(time, y)
+    abline(h = min_meas)
+    abline(v = lag)
+  }
+  #return lagtime
+  return(lag)
+}
 
 # Create function to find stationary time
   #Sliding window with threshold and window parameters
 
+
+# fit exp to points inbetween lag and stat time
