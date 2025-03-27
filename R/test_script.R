02370297707fa1bebcb87fe1959898eb220735fd
Jackson D. Maines <95060649+JacksonDMaines@users.noreply.github.com>
2025-03-26 19:40:11 -0500

    Have a working idea for stattime, idk if it works for non synthetic data

--- R/test_script.R
+++ R/test_script.R
@@ -55,7 +55,7 @@ lag <- lagtime(time = x2, meas = z, differences = test, showplot = TRUE)
 
 
 #first cuttime to after lag time, and log data
-time_l <-  which(time > lag)
+time_l <-  which(x2 > lag)
 meas_l <- log(z[(length(z) - length(time_l) + 1):length(z)])
 plot(time_l, meas_l)
 plot(time_l, meas_l*.1)
@@ -65,10 +65,12 @@ plot(time_l, meas_l*.1)
 what <- diff(meas_l)
 plot(time_l[-length(time_l)], what)
 abline(h = .01)
+for (i in 3) {
+  print(i)
+}
 
 
-
-# why isnt this working?
+     # why isnt this working?
 for (i in (length(time_l) - 2 - 1)) {
   start <- i
   end <- i + 2
@@ -81,20 +83,60 @@ for (i in (length(time_l) - 2 - 1)) {
 }
 # like when i do it manually it should stop way earlier
 # i think "for i in vector" is the issue?
+  # yeah what was right
 
 
 start <- 0
 end <- start
-for (i in (length(time_l) - 2 - 1)) {
+for (i in 1:(length(time_l) - 2 - 1)) {
   start <- start + 1
   end <- start + 2
-  if(all(what[start:end] > .05) == FALSE){
-    print("hey")
-    break
+  if(all(what[start:end] > .05) == TRUE){
+    print(what[i])
   }
 }
 
+# lets rewrite this a bit different
+window  <- 2
+for (i in 1:(length(time_l) - window - 1)) {
+  current_window <- what[i:(i + window)]
+  if (all(current_window > .05)) {
+    print(what[i])
+  }
+}
+    # So these work the same
 
+
+# good now I need to figure out a way to find a threshold
+# so looking at my code, I think its multiplying the differences by by some value then
+
+## find lag
+x = -30:30
+y = exp(x) / (1 + exp(x))
+z = y + rnorm(length(y), 0,.02) + 1
+x2 = 0:60
+test <- differences2(time = x2, meas = z, showplot = TRUE)
+lag <- lagtime(time = x2, meas = z, differences = test, showplot = TRUE)
+
+
+## cut to after lag time and log meas
+time_l <-  x2[which(x2 > lag)]
+meas_l <- log(z[(length(z) - length(time_l) + 1):length(z)])
+meas_ln <- z[(length(z) - length(time_l) + 1):length(z)]
+plot(time_l, meas_l)
+plot(time_l, meas_ln)
+
+## multiple differences by threshold
+threshold <- .01
+whatdiff <- diff(meas_l)
+newdiff <- diff(meas_l) * threshold
+plot(1:32, newdiff)
+
+# just for this synthetic data after the threshold you could do while
+# all(stuff) == TRUE print index, then just output last index?
+
+#alt idea
+# next slope be threshold percentage of last slope
 
 
 
